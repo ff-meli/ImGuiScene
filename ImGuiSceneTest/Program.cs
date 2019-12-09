19f0a3882e763067d6d42385285a217bd2936873
meli <57847713+ff-meli@users.noreply.github.com>
2019-12-09 10:42:17 -0800

    Add testbed project

---
+++ ImGuiSceneTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using ImGuiNET;
+using ImGuiScene;
+using static SDL2.SDL;
+
+namespace ImGuiSceneTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var scene = new SimpleImGuiScene("ImGui Test", fullscreen: true))
+            {
+                scene.Window.MakeWindowTransparent(SimpleSDLWindow.CreateColorKey(0, 0, 0));
+
+                scene.Window.OnSDLEvent += (ref SDL_Event sdlEvent) =>
+                {
+                    if (sdlEvent.type == SDL_EventType.SDL_KEYDOWN && sdlEvent.key.keysym.scancode == SDL_Scancode.SDL_SCANCODE_ESCAPE)
+                    {
+                        scene.ShouldQuit = true;
+                    }
+                    return true;
+                };
+
+                scene.OnBuildUI += () =>
+                {
+                    ImGui.ShowDemoWindow();
+                };
+
+                scene.Run();
+            }
+        }
+    }
+}
