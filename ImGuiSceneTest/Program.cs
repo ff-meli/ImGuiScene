0bf20d86b32df4d601bee9d84d3974b19eeabb07
meli <57847713+ff-meli@users.noreply.github.com>
2020-02-06 09:16:39 -0800

    readd test project

---
+++ ImGuiSceneTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImGuiNET;
+using ImGuiScene;
+
+namespace ImGuiSceneTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var scene = SimpleImGuiScene.CreateOverlay(RendererFactory.RendererBackend.DirectX11))
+            {
+                scene.OnBuildUI += ImGui.ShowDemoWindow;
+                scene.Run();
+            }
+        }
+    }
+}
